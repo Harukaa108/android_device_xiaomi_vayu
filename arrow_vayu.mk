58e263762ff07578096144df3f2e27d1325a82f8
Haruka <149857569+Harukaa108@users.noreply.github.com>
2024-03-12 20:15:55 +0700

    vayu: Fixed Missing Vendor Arrow Common_full.mk

--- arrow_vayu.mk
+++ arrow_vayu.mk
@@ -8,8 +8,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common ArrowOS configuration.
-$(call inherit-product, vendor/arrow/config/common_full_phone.mk)
+# Inherit some common Arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from vayu device
